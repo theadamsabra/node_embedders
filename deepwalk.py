460ac61932fa2f213e22aa3e49cc6cf041b5bdd0
theadamsabra <theadamsabra@gmail.com>
2023-11-03 01:41:04 +0000

    documentation mainly

--- deepwalk.py
+++ deepwalk.py
@@ -7,7 +7,7 @@ class DeepWalk:
         '''
         Core DeepWalk class. Contains main algorithm from paper including SkipGram.
         Inputs:
-            - G (): 
+            - G (): UNSURE OF GRAPH TYPE YET.   
             - win_size (int): window size
             - embedding_size (int): embedding size of output representation
             - walks_per_vertex (int): number of walks per vertex
@@ -22,12 +22,16 @@ class DeepWalk:
         self.seed = seed
 
     def _init_vertex_representations(self):
+        '''Initialize phi - or embedding matrix - to be optimized.'''
         torch_geometric.seed_everything(self.seed) 
         self.phi = torch.rand(
             size = (len(self.G), self.embedding_size)
         )
 
+    def _construct_binary_tree(self):
+        pass
+
     def calculate_embeddings(self):
         '''Main function run to calculate embedding matrix.'''
         self._init_vertex_representations()
-        pass 
+        binary_tree = self._construct_binary_tree() 
