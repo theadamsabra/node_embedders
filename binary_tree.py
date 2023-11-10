1eda2122707a48e0679fa0fe727f6987d6867cde
theadamsabra <theadamsabra@gmail.com>
2023-11-10 02:26:45 +0000

    binary tree init

---
+++ binary_tree.py
@@ -0,0 +1,18 @@
+class Node:
+    '''
+    Node class for binary tree construction.
+    '''
+    def __init__(self) -> None:
+        self.left = None
+        self.right = None
+        self.data = None 
+
+class BinaryTree:
+    '''
+    Binary tree class for Hierarchical Softmax based optimization.  
+    '''
+    def __init__(self) -> None:
+        self.root = Node()
+    
+    def insert(self) -> None:
+        pass
